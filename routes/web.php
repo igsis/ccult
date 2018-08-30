453536d8a4b96611c172945c8aa628f8f9309e1f
Isaías Mendes <isaiaszmendes@gmail.com>
2018-08-30 17:30:38 -0300

    rotas aurh para pf e pj

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,37 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'PessoaFisica'], function(){
+    // Authentication Routes...
+    Route::get('login', 'PessoaFisicaAuth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'PessoaFisicaAuth\LoginController@login');
+    Route::post('logout', 'PessoaFisicaAuth\LoginController@logout');
+    
+    // Registration Routes...
+    Route::get('register', 'PessoaFisicaAuth\RegisterController@showRegistrationForm');
+    Route::post('register', 'PessoaFisicaAuth\RegisterController@register');
+    
+    // Password Reset Routes...
+    Route::get('password/reset', 'PessoaFisicaAuth\ForgotPasswordController@showLinkRequestForm');
+    Route::post('password/email', 'PessoaFisicaAuth\ForgotPasswordController@sendResetLinkEmail');
+    Route::get('password/reset/{token}', 'PessoaFisicaAuth\ResetPasswordController@showResetForm');
+    Route::post('password/reset', 'PessoaFisicaAuth\ResetPasswordController@reset');
+});
+
+Route::group(['prefix' => 'pessoaJuridica'], function(){
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('logout', 'Auth\LoginController@logout');
+    
+    // Registration Routes...
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm');
+    Route::post('register', 'Auth\RegisterController@register');
+    
+    // Password Reset Routes...
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+});
