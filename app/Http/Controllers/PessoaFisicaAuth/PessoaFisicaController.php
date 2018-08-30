9964ebe6c193a5034aaa5d335991d4e1a722429a
Isaías Mendes <isaiaszmendes@gmail.com>
2018-08-30 18:40:47 -0300

    add controller pf e pj

---
+++ app/Http/Controllers/PessoaFisicaAuth/PessoaFisicaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\PessoaFisicaAuth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PessoaFisicaController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/PessoaJuridicaAuth/PessoaJuridicaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\PessoaJuridicaAuth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PessoaJuridicaController extends Controller
+{
+    //
+}
