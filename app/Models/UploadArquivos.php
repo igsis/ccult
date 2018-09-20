ac80d269cfda30716146e61968dbe49df41c670b
Isaías Mendes <isaiaszmendes@gmail.com>
2018-09-20 10:26:55 -0300

    add models para upload de arquivos

---
+++ app/Models/UploadArquivos.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ccult\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UploadArquivos extends Model
+{
+    protected $primaryKey = 'idUploadArquivo';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'idUploadArquivo', 
+        'idTipo', 
+        'idPessoa', 
+        'idListaDocumento', 
+        'arquivo', 
+        'dataEnvio', 
+        'publicado'
+    ];
+
+
+}
